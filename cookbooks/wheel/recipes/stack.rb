2c6eeee26648c444c7aa84c69fc5af5b251ae755
Max Korenkov <mkorenkov@gmail.com>
2012-05-04 14:24:03 +0300

    stack_centos.sh

--- cookbooks/wheel/recipes/stack.rb
+++ cookbooks/wheel/recipes/stack.rb
@@ -9,7 +9,7 @@ include_recipe "wheel::prestack"
 
 Chef::Log.info("started recipe stack")
 
-execute "sudo ./stack.sh" do
+execute "sudo ./stack_centos.sh" do
     environment ({"HOME" => "/home/#{node[:wheel][:username]}"})
     cwd "/home/#{node[:wheel][:username]}/devstack"
     not_if {File.exists?("/opt/stack/devstack")}
