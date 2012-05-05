8193d6498c142e6e9dd1b10827c6650d891dd22d
Max Korenkov <mkorenkov@gmail.com>
2012-05-05 14:06:03 +0300

    use stack.sh again

--- cookbooks/wheel/recipes/stack.rb
+++ cookbooks/wheel/recipes/stack.rb
@@ -10,7 +10,7 @@ include_recipe "wheel::prestack"
 Chef::Log.info("started recipe stack")
 Chef::Log.debug("enabled services: #{node[:stack][:enabled_services]}")
 
-execute "sudo ./stack_centos.sh" do
+execute "sudo ./stack.sh" do
     environment ({"HOME" => "/home/#{node[:wheel][:username]}"})
     cwd "/tmp/#{node[:wheel][:username]}/devstack"
     not_if {File.exists?("/opt/stack/devstack")}
